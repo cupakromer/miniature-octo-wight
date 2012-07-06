2d587fda2cad763300e24a5c93191a12b4d8d491
Aaron Kromer <aaron.kromer@gmail.com>
2012-07-06 18:59:14 -0400

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TravisRails::Application.config.secret_token = '19a773a47cbe57a4645c24d6c7dc65cca04122df3fe0ac900d44a7688977545c7405b457de8bbacfcc64eee8fb55fe37057931b987cf30252cd46d8f4aef7f24'

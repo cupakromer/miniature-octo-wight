267e66ef44ae54c2a52cb752b2f2d60e39d49394
Aaron Kromer <aaron.kromer@gmail.com>
2012-07-06 22:22:56 -0400

    test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe User do
   describe "#send_message" do
     context "when the user is under their subscription limit" do
-      it "sends a message to another user" do
-        zach = User.create!
-        david = User.create!
-        msg = zach.send_message recipient: david, title: "Book Update", text: "Beta 11 includes great stuff!"
-        david.received_messages.should == [msg]
-      end
+#      it "sends a message to another user" do
+#        zach = User.create!
+#        david = User.create!
+#        msg = zach.send_message recipient: david, title: "Book Update", text: "Beta 11 includes great stuff!"
+#        david.received_messages.should == [msg]
+#      end
     end
   end
 end
